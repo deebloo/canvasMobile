ff24fa5954c7301a6eebf5d11b0603eb8248a269
Danny Blue <dannybluedesign@gmail.com>
2014-05-07 20:26:51 -0400

    start adding text

--- js/canvasMobile.js
+++ js/canvasMobile.js
@@ -1,43 +1,67 @@
+/*
+** Drawer class. Accepts an object container height, width, and the appropriate states
+*/
 function Drawer(opts) {
   this.height = opts.height;
   this.width = opts.width;
   this.stage = opts.stage;
+  this.data = opts.data;
 
-  var drawerLayer;
+  // placeholder for the canvas layer
+  this.drawerLayer;
+}
 
-  this.create = function() {
-    drawerLayer = new Kinetic.Layer();
+/*
+** Create method. creates and draws the drawer menu to the canvas and adds the canvas to the staage
+*/
+Drawer.prototype.create = function() {
+  this.drawerLayer = new Kinetic.Layer(); // New layer for the drawer
 
-    var drawerHeight = 0;
+  var drawerHeight = 0; // calculate the total height of the drawer
 
-    for (var i = 0; i <= this.height * 10 + 1; i += this.height + 1) {
-      drawerHeight += this.height + 1;
+  for (var i = 0; i <= this.height * this.data.length + 1; i += this.height + 1) {
+    drawerHeight += this.height + 1;
 
-      var object = new Kinetic.Rect({
-        x: 0,
-        y: i,
-        width: this.width,
-        height: this.height,
-        fill: "grey"
-      });
+    // Create the rectangles for the drawer items
+    var object = new Kinetic.Rect({
+      x: 0,
+      y: i,
+      width: this.width,
+      height: this.height,
+      fill: "grey"
+    });
 
-      drawerLayer.add(object); // add the shape to the layer
-    }
+    var text = new Kinetic.Text({
+      x: 20,
+      y: i + 10,
+      text: "test",
+      fontSize: 30,
+      fontFamily: "Calibri",
+      fill: "white"
+    });
 
-    drawerLayer.offsetX(this.width);
+    this.drawerLayer.add(object); // add the shape to the layer
+    this.drawerLayer.add(text);
+  }
 
-    this.stage.attrs.width = this.width;
-    this.stage.attrs.height = drawerHeight;
+  this.drawerLayer.offsetX(this.width); // offset the drawer by the set width
 
-    this.stage.add(drawerLayer);
-  };
+  // set the canvas element to the height and width of the created drawer
+  this.stage.attrs.width = this.width;
+  this.stage.attrs.height = drawerHeight;
 
-  this.slideOut = function(opts) {
-    var duration = typeof opts !== "undefined" && typeof opts.duration !== "undefined" ? opts.duration : .25;
-    var easing = typeof opts !== "undefined" && typeof opts.easing !== "undefined" ? opts.easing : null;
+  this.stage.add(this.drawerLayer); // add the drawer to the stage
+};
+
+/*
+** Slide method. Animates the drawer menu.
+*/
+Drawer.prototype.slideOut = function(opts) {
+  var duration = typeof opts !== "undefined" && typeof opts.duration !== "undefined" ? opts.duration : .25;
+  var easing = typeof opts !== "undefined" && typeof opts.easing !== "undefined" ? opts.easing : null;
 
     var tween = new Kinetic.Tween({
-      node: drawerLayer,
+      node: this.drawerLayer,
       duration: duration,
       x: this.width,
       y: 0,
@@ -45,5 +69,4 @@ function Drawer(opts) {
     });
 
     tween.play();
-  };
-}
+};
